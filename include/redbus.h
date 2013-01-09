03c7d3d5c93ba36f6e46c5ff6040eb43cb9d8659
Brian Hodgins <fatalnix@gmail.com>
2013-01-09 09:38:12 -0500

    redbus: implementation of external bus functions

--- include/redbus.h
+++ include/redbus.h
@@ -2,11 +2,11 @@
 
 void rb_enable(void);
 void rb_disable(void);
-
+void rb_disable_ext(void);
 void __fastcall__ rb_set_window(void* address);
-
+void __fastcall__ rb_set_window_ext(void* address);
 void __fastcall__ rb_map_device(unsigned char id);
-
+void rb_enable_ext(void);
 
 
 // 0x00 Map device in Reg A to redbus window.
@@ -31,3 +31,4 @@ void __fastcall__ rb_map_device(unsigned char id);
 // 0x86 Get POR address to A
 
 // 0xFF Output A register to MC logfile.
+
